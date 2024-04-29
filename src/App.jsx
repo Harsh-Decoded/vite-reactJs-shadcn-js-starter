8df6d1584ebf485fd3203055a367e0e7347681a4
Harsh-Decoded <gamerharsh0@gmail.com>
2024-04-29 17:27:19 +0530

    SEO optimized

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ import { ModeToggle } from "./components/mode-toggle"
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="h-screen flex justify-center items-center">
-        <ModeToggle/>
+      <div className="h-screen flex flex-col justify-center items-center">
+        <a 
+          href="https://github.com/Harsh-Decoded/vite-reactJs-shadcn-js-starter" 
+          className="underline underline-offset-2 p-2 text-blue-600">
+            GitHub Repository
+        </a>
+        <h1 className="font-medium p-2">Shadcn Dark Button</h1>
+        <ModeToggle />
       </div>
     </ThemeProvider>
   )
